58e281e3a0427c09b229eb1bf816c3e0e8eec730
at-alfredagent <alfredagent@alfredtravel.io>
2026-08-09 11:52:12 +1000

    Separate public GA4 CTA from Firebase events

--- analytics.js
+++ analytics.js
@@ -40,6 +40,5 @@
       campaign_campaign: new URLSearchParams(window.location.search).get('utm_campaign') || undefined,
     };
     window.gtag('event', 'cta_click', eventParams);
-    window.gtag('event', 'webapp_screen_view', eventParams);
   });
 })();
